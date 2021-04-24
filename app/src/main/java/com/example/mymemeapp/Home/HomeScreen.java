edddb3900635a7683d33ae9e92879527b9fc01bf
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2021-04-24 10:58:48 +0530

    added login

---
+++ app/src/main/java/com/example/mymemeapp/Home/HomeScreen.java
@@ -0,0 +1,16 @@
+package com.example.mymemeapp.Home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mymemeapp.R;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
