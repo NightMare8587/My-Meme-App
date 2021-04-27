fc08f8232de0ee8c6baf000535b450e21914790a
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2021-04-27 17:36:39 +0530

    add permission request

--- app/src/main/java/com/example/mymemeapp/MainActivity.java
+++ app/src/main/java/com/example/mymemeapp/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.mymemeapp;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
@@ -15,6 +19,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.developer.kalert.KAlertDialog;
 import com.example.mymemeapp.Home.HomeScreen;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -75,6 +80,10 @@ public class MainActivity extends AppCompatActivity {
         if(currentUser != null){
             startActivity(new Intent(this,HomeScreen.class));
             finish();
+        }
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
         }
         initialise();
         loginUsingNum.setOnClickListener(new View.OnClickListener() {
@@ -215,6 +224,7 @@ public class MainActivity extends AppCompatActivity {
 
                 reference.child("Users").child(Objects.requireNonNull(auth.getUid())).child("number").setValue(number);
                 startActivity(new Intent(getApplicationContext(),HomeScreen.class));
+                finish();
             }
         });
     }
@@ -261,6 +271,7 @@ public class MainActivity extends AppCompatActivity {
                             GoogleSignInDB googleSignInDB = new GoogleSignInDB(account.getDisplayName(),account.getEmail());
                             reference.child("Users").child(user.getUid()).setValue(googleSignInDB);
                             startActivity(new Intent(MainActivity.this, HomeScreen.class));
+                            finish();
 //                            updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
@@ -269,6 +280,34 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 1){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+            else{
+                new KAlertDialog(MainActivity.this,KAlertDialog.ERROR_TYPE)
+                        .setTitleText("Permission Required")
+                        .setContentText("Without Storage Permission You can't share memes\nWanna provide permission??")
+                        .setConfirmText("Yes")
+                        .setCancelText("No")
+                        .setConfirmClickListener(new KAlertDialog.KAlertClickListener() {
+                            @Override
+                            public void onClick(KAlertDialog kAlertDialog) {
+                                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+                                kAlertDialog.dismissWithAnimation();
+                            }
+                        }).setCancelClickListener(new KAlertDialog.KAlertClickListener() {
+                    @Override
+                    public void onClick(KAlertDialog kAlertDialog) {
+                        kAlertDialog.dismissWithAnimation();
+                    }
+                }).show();
+            }
+        }
     }
 
     private void getSignInInformation() {
